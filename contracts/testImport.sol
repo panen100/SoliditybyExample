ed12fa38996bb7265e6d860593594210014ee41f
panen100 <en.pan@outlook.com>
2024-11-28 22:55:12 +0800

    new a testImport.sol

---
+++ contracts/testImport.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import '@openzeppelin/contracts/access/Ownable.sol';
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+
+contract testImport{
+    uint8 public a;
+    uint8 public a1;
+}
